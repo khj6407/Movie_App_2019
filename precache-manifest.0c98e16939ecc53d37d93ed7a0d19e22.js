abff957119ace0351b0448fd71f510067c1afc40
kimHJ <khj64076407@gmail.com>
2019-12-22 21:57:26 +0900

    Updates

---
+++ precache-manifest.0c98e16939ecc53d37d93ed7a0d19e22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc04fc1ceecb149c13725469b9e1992",
+    "url": "/Movie_App_2019/index.html"
+  },
+  {
+    "revision": "37ddf2ac02228d80e9de",
+    "url": "/Movie_App_2019/static/css/main.63531b48.chunk.css"
+  },
+  {
+    "revision": "a60557ab49ae9a27434e",
+    "url": "/Movie_App_2019/static/js/2.2544e078.chunk.js"
+  },
+  {
+    "revision": "37ddf2ac02228d80e9de",
+    "url": "/Movie_App_2019/static/js/main.2e51a00f.chunk.js"
+  },
+  {
+    "revision": "328078f05e78067f5f20",
+    "url": "/Movie_App_2019/static/js/runtime-main.74f4e633.js"
+  }
+]);
